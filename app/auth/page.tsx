1577758caa62df73ed2ad8d5ea65eabab816bf0d
DaleHarveyBacaoco <166678656+DaleHarveyBacaoco@users.noreply.github.com>
2026-04-30 03:04:28 +0800

    LAst

---
+++ app/auth/page.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { useState } from "react";
+import { supabase } from "../../lib/supabaseClient";
+
+export default function AuthPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const login = async () => {
+    const { error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+
+    if (error) alert(error.message);
+    else window.location.href = "/dashboard";
+  };
+
+  const signUp = async () => {
+    const { error } = await supabase.auth.signUp({
+      email,
+      password,
+    });
+
+    if (error) alert(error.message);
+    else alert("Check your email!");
+  };
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h2>Login / Sign Up</h2>
+
+      <input
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <br /><br />
+
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <br /><br />
+
+      <button onClick={login}>Login</button>
+      <button onClick={signUp}>Sign Up</button>
+    </div>
+  );
+}
