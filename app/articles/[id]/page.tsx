b99186a0a3fe6e7ccb2ab6eba5088d41cf8435fb
DaleHarveyBacaoco <166678656+DaleHarveyBacaoco@users.noreply.github.com>
2026-05-10 23:24:04 +0800

    Top 5

--- app/articles/[id]/page.tsx
+++ app/articles/[id]/page.tsx
@@ -13,7 +13,10 @@ type Comment = {
 };
 
 export default function ArticleDetail() {
-  const { id } = useParams();
+  const params = useParams();
+  const id = Array.isArray(params.id)
+  ? params.id[0]
+  : params.id;
 
   const [article, setArticle] = useState<any>(null);
   const [comments, setComments] = useState<Comment[]>([]);
