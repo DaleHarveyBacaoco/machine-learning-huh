0407f9cdf6608cad8c662d3fa2d1b24f596dddf8
DaleHarveyBacaoco <166678656+DaleHarveyBacaoco@users.noreply.github.com>
2026-05-11 23:25:01 +0800

    Top 5

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
-        background: "linear-gradient(to right, #626262ff, #86d5ffff)",
+        background: "linear-gradient(to right, #626262ff, #ffffffff)",
         color: "white",
         textAlign: "center",
         padding: "20px",
@@ -20,7 +20,7 @@ export default function Home() {
     >
       <div
         style={{
-          background: "rgba(255, 255, 255, 0.87)",
+          background: "rgba(108, 159, 255, 0.87)",
           padding: "40px",
           borderRadius: "12px",
           backdropFilter: "blur(10px)",
